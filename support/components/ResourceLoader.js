82754c2d7ffa1a4d783fc2386d4720ef181cef36
langj123 <88846488+web-redegg-marketing@users.noreply.github.com>
2023-08-29 09:00:27 -0600

    filters update

--- support/components/ResourceLoader.js
+++ support/components/ResourceLoader.js
@@ -79,7 +79,6 @@ const ResourceLoader = () => {
 const ResourceFilters = (props) => {
 
   const { taxonomies, currentFilter, currentTax, filterCats, filterMin, filterMax, searchFilter, orderFilter, selectedValues } = props;
-  console.log(taxonomies);
   return (
       <Fragment>
           <div className="search-cont">
