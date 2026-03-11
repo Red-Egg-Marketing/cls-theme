a555b91049eb399ffb38ef5dc096dc580ff92b75
langj123 <88846488+web-redegg-marketing@users.noreply.github.com>
2026-03-11 10:57:21 -0600

    fix for footer

--- footer.php
+++ footer.php
@@ -11,16 +11,18 @@
 if (function_exists('get_field')) {
     global $wpdb;
     $company_settings = [
-        'phone'         => get_field('business_phone', 'options'),
-        'email'         => get_field('business_email', 'options'),
-        'street'        => get_field('business_street', 'options'),
-        'city'          => get_field('business_city', 'options'),
-        'zip'           => get_field('business_zip', 'options'),
-        'state'         => get_field('business_state', 'options'),
-        'areas'         => get_permalink(get_field('areas_served', 'options')),
-        'icons'         => get_field('icons', 'options'),
-        'disclaimer'    => get_field('disclaimer', 'options'),
-        'logo'          => get_field('footer_logo', 'options')
+        'phone'                 => get_field('business_phone', 'options'),
+        'email'                 => get_field('business_email', 'options'),
+        'street'                => get_field('business_street', 'options'),
+        'city'                  => get_field('business_city', 'options'),
+        'zip'                   => get_field('business_zip', 'options'),
+        'state'                 => get_field('business_state', 'options'),
+        'areas'                 => get_permalink(get_field('areas_served', 'options')),
+        'icons'                 => get_field('icons', 'options'),
+        'disclaimer'            => get_field('disclaimer', 'options'),
+        'logo'                  => get_field('footer_logo', 'options'),
+        'multiple_addresses'    => get_field('multiple_addresses', 'options'),
+        'addresses'             => get_field('addresses', 'options')
     ];
 
      
@@ -30,6 +32,7 @@ if (function_exists('get_field')) {
         <div class="site-info">
             <div class="wrapper">
                 <div class="col">
+                    <?php if (!$company_settings['multiple_addresses']) { ?>
                     <address>
                         <p style="margin-bottom: 10px">
                             <?= $company_settings['street'] . ', ' . $company_settings['city'] . ', ' . $company_settings['state'] . ' ' . $company_settings['zip'] ?><br />
@@ -38,19 +41,33 @@ if (function_exists('get_field')) {
                             <a href="tel:<?= $company_settings['phone']; ?>" class="contact-link"><?= $company_settings['phone'] ?></a><br />
                         </p>
                     </address>
-                     <ul class="social">
-                        <?php
-                        if (is_array($company_settings['icons']) && sizeof($company_settings['icons']) > 0) {
-                            foreach($company_settings['icons'] as $icon) {
-                                $src = $icon['social']['link'];
-                                $class = $icon['social']['icon_class'];
-                                ?>
-                                    <li><a href="<?= $src; ?>" class="fa-brands fa-<?= $class; ?>" target="_blank"></a></li>
-                                <?php
-                            }
-                        }
+                    <?php } elseif($company_settings['multiple_addresses'] == true) { 
+                        $addresses = $company_settings['addresses'];
+                        foreach($addresses as $address) {
+                            $title = $address['location_address']['location_title'];
+                            $street = $address['location_address']['street'];
+                            $zip = $address['location_address']['zip'];
+                            $city = $address['location_address']['city'];
+                            $state = $address['location_address']['state'];
+                            $phone = $address['location_address']['phone'];
                         ?>
-                    </ul>
+                                <address>
+                                    
+                                    <p style="margin-bottom: 20px;">
+                                        <?php if ($title != '') { ?>
+                                            <strong><?php echo $title; ?></strong><br />
+                                        <?php } ?>
+                                        <?php if ($phone != '') { ?>
+                                            <a href="tel:<?= $phone ?>" class="contact-link"><?= $phone ?></a>
+                                        <?php } ?>
+                                        <?= $street . ', <br />' . $city . ', ' . $state . ' ' . $zip ?><br />
+                                    </p>
+                                </address>
+                        <?php
+                        }
+                     }
+                     ?>
+                     
                 </div>
                 <div class="col">
                     <a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home" class="footer-home">
@@ -66,6 +83,19 @@ if (function_exists('get_field')) {
                         )
                     );
                     ?>
+                    <ul class="social" style="margin-top: 25px;">
+                        <?php
+                        if (is_array($company_settings['icons']) && sizeof($company_settings['icons']) > 0) {
+                            foreach($company_settings['icons'] as $icon) {
+                                $src = $icon['social']['link'];
+                                $class = $icon['social']['icon_class'];
+                                ?>
+                                    <li><a href="<?= $src; ?>" class="fa-brands fa-<?= $class; ?>" target="_blank"></a></li>
+                                <?php
+                            }
+                        }
+                        ?>
+                    </ul>
                 </div>
             </div>
             <div class="footer-menu">
@@ -81,6 +111,7 @@ if (function_exists('get_field')) {
                         )
                     );
                 ?>
+
             </div>
         </div><!-- .site-info -->
     </footer><!-- #colophon -->
