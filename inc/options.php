83933d55c605e783c0a0ddf93f47c1bfcbee56dd
langj123 <88846488+web-redegg-marketing@users.noreply.github.com>
2026-05-08 11:46:10 -0600

    Added pricing info for doc fee

--- inc/options.php
+++ inc/options.php
@@ -1,6 +1,17 @@
 <?php
 // Add options page for site
 
+ add_filter('acf/format_value', function ($value, $post_id, $field) {
+        if (!is_string($value)) return $value;
+
+        $tokens = [
+            '%doc_fee%' => get_option('options_doc_fee'),
+        ];
+
+        return str_replace(array_keys($tokens), array_values($tokens), $value);
+        
+    }, 20, 3);
+
 if( function_exists('acf_add_options_page') ) {
     
 	// Add parent.
@@ -9,5 +20,7 @@ if( function_exists('acf_add_options_page') ) {
         'menu_title'  => __('CLS Site Settings'),
         'redirect'    => false,
      ));
+
+
 
 }
